01f70f8d46898f294dabda3ff14ffe230d4fef56
Camila Santos <camila.souza@servicenet.com.br>
2024-08-19 22:03:36 -0300

    novos componentes

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,35 @@
+import P from 'prop-types';
+import * as Styled from './styles';
+import { SectionContainer } from '../SectionContainer';
+import { LogoLink } from '../LogoLink';
+import { NavLinks } from '../NavLinks'
+import { Menu as MenuIcon } from '@styled-icons/material-outlined/Menu';
+import { useState } from 'react';
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
+
+
+export const Menu = ({ links = [], logoData }) => {
+    const [visible, setVisibles] = useState(false)
+
+    return (
+        <>
+            <Styled.Button onClick={() => setVisibles(!visible)}>
+                {visible ? <CloseIcon /> : <MenuIcon />  }
+            </Styled.Button>
+
+            <Styled.Container visible={visible}>
+                <SectionContainer>
+                    <Styled.MenuContainer>
+                        <LogoLink {...logoData} />
+                        <NavLinks links={links} />
+                    </Styled.MenuContainer>
+                </SectionContainer>
+            </Styled.Container>
+        </>
+    );
+};
+
+Menu.propTypes = {
+    ...NavLinks.propTypes,
+    logoData: P.shape(LogoLink.propTypes).isRequired
+};
---
+++ src/components/Menu/stories.jsx
@@ -0,0 +1,23 @@
+import { Menu } from '.';
+import linksMock from '../NavLinks/mock'
+
+export default {
+    title: 'Menu',
+    component: 'Menu',
+    args: {
+        links: linksMock,
+        logoData: {
+            text: 'Logo',
+            link: '#target',
+            srcImg: ''
+        }
+},
+    argTypes: {
+    children: { type: 'string' },
+    }
+};
+export const Template = (args) => {
+    return (
+        <Menu {...args} />
+    );
+};
